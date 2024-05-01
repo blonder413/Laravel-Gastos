65a6d045a349c778932a565fc8d723dff0ff5a35
Ingeniero Morales <blonder413@gmail.com>
2024-05-01 15:43:12 -0500

    feat: colores por rango de precios para grafico

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,17 +36,9 @@
         </p>
     </div>
     @php
-    if ($total < 2000000) {
-        $class = 'text-success';
-    } elseif ($total < 4000000) {
-        $class = 'text-warning';
-    } else {
-        $class = 'text-danger';
-    }
-    @endphp
-    <div class="card-footer text-body-secondary d-flex justify-content-center {{ $class }} fw-bold">
+    if ($total < 2000000) { $class='text-success' ; } elseif ($total < 3000000) { $class='text-warning' ; } else { $class='text-danger' ; } @endphp <div class="card-footer text-body-secondary d-flex justify-content-center {{ $class }} fw-bold">
         $ {{ number_format($total, 0, ',', '.') }}
-    </div>
+</div>
 </div>
 @endsection
 
@@ -56,89 +48,101 @@
     const datos = <?= json_encode($gastos_grafico) ?>;
     let fechas = [];
     let valores = [];
+    var coloresBorde = []
+    var coloresFondo = []
     for (let i = 0; i < datos.length; i++) {
         fechas[i] = datos[i].mes;
         valores[i] = datos[i].total;
+        if (valores[i] < 2000000) {
+            coloresFondo.push('rgba(75, 192, 192, 0.2)'); // verde
+            coloresBorde.push('rgba(75, 192, 192, 1)');
+        } else if (valores[i] < 4000000) {
+            coloresFondo.push('rgba(255, 159, 64, 0.2)'); // verde
+            coloresBorde.push('rgba(255, 159, 64, 1)');
+        } else {
+            coloresFondo.push('rgba(255, 99, 132, 0.2)'); // rojo
+            coloresBorde.push('rgba(255, 99, 132, 1)');
+        }
     }
     // https://www.chartjs.org/docs/latest/
     const myChart = new Chart(ctx, {
-            type: 'bar',    // bar, line, doughnut, pie, radar
-            data: {
-                labels: fechas,
-                datasets: [
-                    {
-                        //barPercentage: 0.5,
-                        //categoryPercentage: 0.5,
-                        label: 'Pesos',
-                        data: valores,
-                        backgroundColor: [
-                            //'rgba(255, 99, 132, 0.2)',  // rojo
-                            //'rgba(54, 162, 235, 0.2)',  // azul
-                            'rgba(255, 206, 86, 0.2)',  // amarillo
-                            //'rgba(75, 192, 192, 0.2)',  // verde
-                            //'rgba(153, 102, 255, 0.2)', // violeta
-                            //'rgba(255, 159, 64, 0.2)'   // naranja
-                        ],
-                        borderColor: [
-                            //'rgba(255, 99, 132, 1)',
-                            //'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            //'rgba(75, 192, 192, 1)',
-                            //'rgba(153, 102, 255, 1)',
-                            //'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1,
-                        barThickness: 50,
-                        maxBarThickness: 75,
-                        minBarLength: 35,
-                    },
-                ]
+        type: 'bar', // bar, line, doughnut, pie, radar
+        data: {
+            labels: fechas,
+            datasets: [{
+                //barPercentage: 0.5,
+                //categoryPercentage: 0.5,
+                label: 'Pesos',
+                data: valores,
+                backgroundColor: coloresFondo,
+                // backgroundColor: [
+                    //'rgba(255, 99, 132, 0.2)',  // rojo
+                    //'rgba(54, 162, 235, 0.2)',  // azul
+                    // 'rgba(255, 206, 86, 0.2)', // amarillo
+                    //'rgba(75, 192, 192, 0.2)',  // verde
+                    //'rgba(153, 102, 255, 0.2)', // violeta
+                    //'rgba(255, 159, 64, 0.2)'   // naranja
+                // ],
+                borderColor: coloresBorde,
+                // borderColor: [
+                    //'rgba(255, 99, 132, 1)',
+                    //'rgba(54, 162, 235, 1)',
+                    // 'rgba(255, 206, 86, 1)',
+                    //'rgba(75, 192, 192, 1)',
+                    //'rgba(153, 102, 255, 1)',
+                    //'rgba(255, 159, 64, 1)'
+                // ],
+                borderWidth: 1,
+                barThickness: 50,
+                maxBarThickness: 75,
+                minBarLength: 35,
+            }, ]
+        },
+        options: {
+            //animations: {               // para tipo line
+            //    tension: {
+            //        duration: 1000,
+            //        easing: 'linear',
+            //        from: 1,
+            //        to: 0,
+            //        loop: true
+            //    }
+            //},
+            // animation: true,
+            //aspectRatio: 1,
+            //indexAxis: 'y',
+            maintainAspectRatio: true,
+            //spanGaps: true,   // para tipo line
+            //showLine: false,  // para tipo line
+            parsing: {
+                yAxisKey: 'Gastos',
             },
-            options: {
-                //animations: {               // para tipo line
-                //    tension: {
-                //        duration: 1000,
-                //        easing: 'linear',
-                //        from: 1,
-                //        to: 0,
-                //        loop: true
-                //    }
-                //},
-                // animation: true,
-                //aspectRatio: 1,
-                //indexAxis: 'y',
-                maintainAspectRatio: true,
-                //spanGaps: true,   // para tipo line
-                //showLine: false,  // para tipo line
-                parsing: {
-                    yAxisKey: 'Gastos',
-                },
-                plugins: {
-                    title: {
-                        display: true,
-                        text: 'Gastos del último año'
-                    }
-                },
-                scales: {
-                    x:{
-                        // stacked: true,      // poner ambos arreglos en una sola barra, usar en ambos ejes
-                        grid: {
-                            display: false  // ocultar las líneas guía
-                        }
-                    },
-                    y: {
-                        //beginAtZero: true,
-                        // stacked: true,      // poner ambos arreglos en una sola barra, usar en ambos ejes
-                        grid: {
-                            display: false  // ocultar las líneas guía
-                        },
-                        ticks: {
-                            display: true  // ocultar las etiquetas del eje y
-                        }
-                    },
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Gastos del último año'
                 }
             },
-            // plugins: [ChartDataLabels]
-        });
+            scales: {
+                x: {
+                    // stacked: true,      // poner ambos arreglos en una sola barra, usar en ambos ejes
+                    grid: {
+                        display: false // ocultar las líneas guía
+                    }
+                },
+                y: {
+                    //beginAtZero: true,
+                    // stacked: true,      // poner ambos arreglos en una sola barra, usar en ambos ejes
+                    grid: {
+                        display: false // ocultar las líneas guía
+                    },
+                    ticks: {
+                        display: true // ocultar las etiquetas del eje y
+                    }
+                },
+            }
+        },
+        // plugins: [ChartDataLabels]
+    });
 </script>
 @endsection
